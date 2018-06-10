d270100947d0434e82c49b8c472e13212e234fc4
zhengyuaing <wang_zhengya@163.com>
2018-06-10 16:47:33 +0800

    init

--- ding.js
+++ ding.js
@@ -19,17 +19,33 @@ $.get("http://192.168.31.140:3000/api/jsapi-oauth", function(data){
 
     dd.ready(function(){
         $('h1').html("success");
-        dd.biz.contact.createGroup({
-            corpId: data.corpId, //企业id，可选，配置该参数即在指定企业创建群聊天
-            users: [], //默认选中的用户工号列表，可选；使用此参数必须指定corpId
+        dd.biz.contact.complexPicker({
+            title:"测试标题",            //标题
+            corpId:data.corpId,              //企业的corpId
+            multiple:true,            //是否多选
+            limitTips:"超出了",          //超过限定人数返回提示
+            maxUsers:100,            //最大可选人数
+            pickedUsers:[],            //已选用户
+            pickedDepartments:[],          //已选部门
+            disabledUsers:[],            //不可选用户
+            disabledDepartments:[],        //不可选部门
+            requiredUsers:[],            //必选用户（不可取消选中状态）
+            requiredDepartments:[],        //必选部门（不可取消选中状态）
+            appId:'178631091',              //微应用的Id
+            permissionType:GLOBAL,          //选人权限，目前只有GLOBAL这个参数
+            responseUserOnly:false,        //返回人，或者返回人和部门
+            startWithDepartmentId:0 ,   // 0表示从企业最上层开始
             onSuccess: function(result) {
-                /*{
-                    id: 123   //企业群id
-                }*/
-                $('p#err').html(result);
+                /**
+                {
+                    selectedCount:1,                              //选择人数
+                    users:[{"name":"","avatar":"","emplId":""}]，//返回选人的列表，列表中的对象包含name（用户名），avatar（用户头像），emplId（用户工号）三个字段
+                    departments:[{"id":,"name":"","number":}]//返回已选部门列表，列表中每个对象包含id（部门id）、name（部门名称）、number（部门人数）
+                }
+                */
+               $('p#err').html(JSON.stringify(result));
             },
-            onFail: function(err) {
-            }
+           onFail : function(err) {}
         });
     });
 
