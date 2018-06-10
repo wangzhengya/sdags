f465fc4e1c93bed34abb275b4f76b97e7fd9a1b3
zhengyuaing <wang_zhengya@163.com>
2018-06-10 15:49:09 +0800

    init

--- dingpc.js
+++ dingpc.js
@@ -1,1 +1,29 @@
 
+$('h1').html("开始校验jsapi。。。");
+$.get("http://192.168.31.140:3000/api/jsapi-oauth", function(data){
+    console.dir(data);
+    console.log(window.location.href);
+    DingTalkPC = {
+        agentId: '178631091', // 必填，微应用ID
+        corpId:  data.corpId,//必填，企业ID
+        timeStamp: data.timeStamp, // 必填，生成签名的时间戳
+        nonceStr: data.nonceStr, // 必填，生成签名的随机串
+        signature: data.signature, // 必填，签名
+        type:0,   //选填，0表示微应用的jsapi，1表示服务窗的jsapi，不填默认为0。该参数从dingtalk.js的0.8.3版本开始支持
+        jsApiList : [ 'runtime.info', 'biz.contact.choose','biz.user.get',
+            'device.notification.confirm', 'device.notification.alert',
+            'device.notification.prompt', 'biz.ding.post',
+            'biz.util.openLink' ,'biz.contact.complexPicker'] // 必填，需要使用的jsapi列表，注意：不要带dd。
+    };
+    $('p#data').html(JSON.stringify(ddConfig));
+    DingTalkPC.config(ddConfig);
+
+    DingTalkPC.ready(function(){
+        $('h1').html("success");
+    });
+
+    DingTalkPC.error(function(err) {
+        $('p#err').html(JSON.stringify(err));
+    });
+
+});
