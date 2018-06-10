0b59438da913e22b0d20d226b91d7397de21f7ec
zhengyuaing <wang_zhengya@163.com>
2018-06-10 16:21:35 +0800

    init

--- dingpc.js
+++ dingpc.js
@@ -8,7 +8,7 @@ $.get("http://192.168.31.140:3000/api/jsapi-oauth", function(data){
         corpId:  data.corpId,//必填，企业ID
         timeStamp: data.timeStamp, // 必填，生成签名的时间戳
         nonceStr: data.nonceStr, // 必填，生成签名的随机串
-        signature: "data.signature", // 必填，签名
+        signature: data.signature, // 必填，签名
         type:0,   //选填，0表示微应用的jsapi，1表示服务窗的jsapi，不填默认为0。该参数从dingtalk.js的0.8.3版本开始支持
         jsApiList : [ 'runtime.info', 'biz.contact.choose','biz.user.get',
             'device.notification.confirm', 'device.notification.alert',
