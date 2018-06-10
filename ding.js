fe7a06e96cb7b2eeefa67f91d5c4b1a6c0041f1d
zhengyuaing <wang_zhengya@163.com>
2018-06-10 18:43:53 +0800

    a

--- ding.js
+++ ding.js
@@ -12,34 +12,20 @@ $.get("http://192.168.31.140:3000/api/jsapi-oauth", function(data){
         jsApiList : [ 'runtime.info', 'biz.contact.choose','biz.user.get',
             'device.notification.confirm', 'device.notification.alert',
             'device.notification.prompt', 'biz.ding.post',
-            'biz.util.openLink' ,'biz.contact.complexPicker','biz.util.scanCard'] // 必填，需要使用的jsapi列表，注意：不要带dd。
+            'biz.util.openLink' ,'biz.contact.complexPicker','ui.pullToRefresh.enable'] // 必填，需要使用的jsapi列表，注意：不要带dd。
     };
     $('p#data').html(JSON.stringify(ddConfig));
     dd.config(ddConfig);
 
     dd.ready(function(){
         $('h1').html("success2");
-        dd.biz.util.scanCard({ // 无需传参数
-            onSuccess: function(data) {
-                $('p#err').html(JSON.stringify(data));
-            //onSuccess将在扫码成功之后回调
-              /* data结构
-               {
-                 "ADDRESS": "深圳市南山区软件产业基地", 
-                 "COMPANY": "深圳市李乔科技有限公司", 
-                 "NAME": "李乔",
-                 "MPHONE": "861333567890",  
-                 "PHONE": "01087654321", 
-                 "POSITION": "CEO", 
-                 "IMAGE": "http://www.taobao.com/xxx.jpg", 
-                 "dt_tranfer": "BusinessCard", 
-                 "request_id": "20161206144554_efd40582d477a29df2e3bc62c260cdae"
-              }
-              */
+        dd.ui.pullToRefresh.enable({
+            onSuccess: function() {
             },
-           onFail : function(err) {
-           }
+            onFail: function() {
+            }
         })
+         
     });
 
     dd.error(function(err) {
